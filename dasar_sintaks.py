fde1211a6cc7bbe8d45b950b2fc2a1bb30c5b84d
rizky-pesat <rizkysmppesat@gmail.com>
2022-07-10 16:00:15 +0700

    initial commit

--- dasar_sintaks.py
+++ dasar_sintaks.py
@@ -5,7 +5,10 @@ print('ibu menjawab, "beli 1 botol susu, kalau ada telur beli 6"')
 print('maka budi berangkat ke toko dan mulai berbelanja')
 
 # Percabangan
-botol_susu = 100
+botol_susu = 123
+telur = 1000
+print(f"jumlah botol susu adalah {botol_susu} botol")
+print(f"jumlah telurnya adalah {telur} telur")
 
 if botol_susu > 0:
     print("budi membeli 1 botol susu")
