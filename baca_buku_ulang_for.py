a258abca48eb5c6fa3f24de9f0b0f0270bc6b848
rizky-pesat <rizkysmppesat@gmail.com>
2022-07-12 03:15:04 +0700

    ini adalah buku ulang

---
+++ baca_buku_ulang_for.py
@@ -0,0 +1,14 @@
+"""
+Program pengulangan baca buku
+
+"""
+
+jml_buku = 100
+print('Ibu berkata "Baca semua bukumu"')
+
+jml_buku_yg_dibaca = 0
+print(f"jumlah buku yang sudah dibaca adalah {jml_buku_yg_dibaca}")
+for jml_buku_yg_dibaca in range(1, jml_buku+1):
+    print(f"buku ke {jml_buku_yg_dibaca} sudah dibaca")
+
+print(f"jumlah buku yang sudah dibaca sekarang adalah {jml_buku_yg_dibaca}")
