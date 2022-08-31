4239f58fd9bde02278569eb28ac9a2c474e6f2ec
rizky-pesat <rizkysmppesat@gmail.com>
2022-08-31 17:38:33 +0700

    tipe data

---
+++ dasar_dictionary.py
@@ -0,0 +1,32 @@
+users = {
+    'id': 1,
+    'name': 'Leanne Graham',
+    'username': 'Bret',
+    'email': 'Sincere@april.biz',
+    'address': {
+      'street': 'Kulas Light',
+      'suite': 'Apt. 556',
+      'city': 'Gwenborough',
+      'zipcode': '92998-3874',
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    }
+}
+print(users)
+print(users['id'])
+print(users['name'])
+print(users['username'])
+print(users['email'])
+print(users['address'])
+print(users['address']['street'])
+print(users['address']['geo']['lat'])
+
+print('\nubahkeJSON\n')
+import json
+result = json.dumps(users)
+print(result)
+
+with open('result.json', 'w') as file:
+    json.dump(users, file)
--- tipe_data.py
+++ tipe_data.py
@@ -24,3 +24,52 @@ for buku2 in daftar_buku2:
 for i in range(0,len(daftar_buku)):
     print(daftar_buku[i])
 
+print('\n*CLear list*')
+daftar_buku.clear()
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nGanti Elemen Pertama')
+daftar_buku = ['MTK','IPA','IPS','ING','IND','PKN']
+daftar_buku[0] = 'ALJBR'
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nAmbil buku elemen kedua')
+buku = daftar_buku.pop(1)
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nBuku yang diambil tadi')
+print(buku)
+
+
+print('\npop doang')
+daftar_buku.pop()
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPerintah Del')
+daftar_buku = ['MTK','IPA','IPS','ING','IND','PKN']
+del daftar_buku[0]
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPerintah Del list comp')
+daftar_buku = ['MTK','IPA','IPS','ING','IND','PKN']
+del daftar_buku[:]
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPerintah Del list comp dari list kedua')
+daftar_buku = ['MTK','IPA','IPS','ING','IND','PKN']
+del daftar_buku[1:]
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPerintah Del menampilkan  genap')
+daftar_buku = ['MTK','IPA','IPS','ING','IND','PKN']
+del daftar_buku[1::2]
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
